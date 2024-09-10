ba0659d9d1aeae9440360a198bc159e584897bad
Fluffymarmot <fluffymarmot@gmail.com>
2024-09-10 18:13:00 +0700

    TroubleShooting_4

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -37,63 +37,73 @@ public class VariablesTheme {
         System.out.println("  JJ  a     a  V  a     a");
 
         System.out.println("\n 4. Вывод min и max значений целых числовых типов");
-        int maxInt = 2147483647;
+        int maxInt = Integer.MAX_VALUE;
         System.out.println("Переменная исходная int: " + maxInt);
         System.out.println("Переменная int инкремент: " + ++maxInt);
         System.out.println("Переменная int декремент: " + --maxInt);
-        byte maxByte = 127;
+        byte maxByte = Byte.MAX_VALUE;
         System.out.println("\nПеременная исходная byte: " + maxByte);
         System.out.println("Переменная byte инкремент: " + ++maxByte);
         System.out.println("Переменная byte декремент: " + --maxByte);
-        short maxShort = 32767;
+        short maxShort = Short.MAX_VALUE;
         System.out.println("\nПеременная исходная short: " + maxShort);
         System.out.println("Переменная short инкремент: " + ++maxShort);
         System.out.println("Переменная short декремент: " + --maxShort);
-        long maxLong = 9223372036854775807L;
+        long maxLong = Long.MAX_VALUE;
         System.out.println("\nПеременная исходная long: " + maxLong);
         System.out.println("Переменная long инкремент: " + ++maxLong);
         System.out.println("Переменная long декремент: " + --maxLong);
-        char maxChar = 65535;
-        System.out.println("\nПеременная исходная char: " + maxChar);
-        System.out.println("Переменная char инкремент: " + ++maxChar);
-        System.out.println("Переменная char декремент: " + --maxChar);
+        char maxChar = Character.MAX_VALUE;
+        int numericMaxChar = (int) maxChar;
+        System.out.println("\nПеременная исходная char: " + numericMaxChar);
+        System.out.println("Переменная char инкремент: " + ++numericMaxChar);
+        System.out.println("Переменная char декремент: " + --numericMaxChar);
 
         System.out.println("\n 5. Перестановка значений переменных.");
         int a = 2;
         int b = 5;
         System.out.println("Исходные значения переменных:" + " " + a + " " + b);
-        int swap = b;
-        System.out.println("Первый способ, при помощи третьей переменной:" + swap + " " + a);
-        a = a * 2 + 1;
-        b = b / 2;
+        int swap = a;
+        a = b;
+        b = swap;
+        System.out.println("Первый способ, при помощи третьей переменной:" + a + " " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
         System.out.println("Второй способ, при помощи арифметических операций:" + a + " " + b);
-        a = b ^ a ^ b;
-        b = a ^ b ^ a; 
+        a = a ^ b;
+        b = b ^ a;
+        a = a ^ b;
         System.out.println("Третий способ , с помощью побитовой операции ^:" + a + " " + b);
 
         System.out.println("\n 6. Вывод символов и их кодов.");
-        char symbolDollar = '$';
-        char symbolAsterisk = '*';
-        char symbolAtSign = '@';
-        char symbolBackslash = '\\';
-        char symbolTilde = '~';
-        System.out.println("- 36 - " + symbolDollar);
-        System.out.println("- 42 - " + symbolAsterisk);
-        System.out.println("- 64 - " + symbolAtSign);
-        System.out.println("- 124 - " + symbolBackslash);
-        System.out.println("- 126 - " + symbolTilde);
+        char dollar = '$';
+        int codeDollar = (int) dollar;
+        char asterisk = '*';
+        int codeAsterisk = (int) asterisk;
+        char atSign = '@';
+        int codeAtSign = (int) atSign;
+        char backslash = '\\';
+        int codeBackslash = (int) backslash;
+        char tilde = '~';
+        int codeTilde = (int) tilde;
+        System.out.println("- " + codeDollar + " - " + dollar);
+        System.out.println("- " + codeAsterisk + " - " + asterisk);
+        System.out.println("- " + codeAtSign + " - " + atSign);
+        System.out.println("- " + codeBackslash + " - " + backslash);
+        System.out.println("- " + codeTilde + " - " + tilde);
 
         System.out.println("\n 7. Вывод в консоль ASCII-арт Дюка.");
-        char symbolSlash = '/';
-        char symbolUnderscore = '_';
-        char symbolLeftParenthesis = '(';
-        char symbolRightParenthesis = ')';
-        System.out.println("    " + symbolSlash + symbolBackslash);
-        System.out.println("   " + symbolSlash + "  " + symbolBackslash);
-        System.out.println("  " + symbolSlash + symbolUnderscore + symbolLeftParenthesis + " " + 
-                symbolRightParenthesis + symbolBackslash);
-        System.out.println(" " + symbolSlash + symbolUnderscore + symbolUnderscore + symbolUnderscore + 
-                symbolSlash + symbolBackslash + symbolUnderscore + symbolBackslash);
+        char slash = '/';
+        char underscore = '_';
+        char leftParenthesis = '(';
+        char rightParenthesis = ')';
+        System.out.println("    " + slash + backslash);
+        System.out.println("   " + slash + "  " + backslash);
+        System.out.println("  " + slash + underscore + leftParenthesis + " " + 
+                rightParenthesis + backslash);
+        System.out.println(" " + slash + underscore + underscore + underscore + 
+                slash + backslash + underscore + backslash);
 
         System.out.println("\n 8. Манипуляции с сотнями, десятками и единицами числа.");
         int baseNum = 123;
@@ -111,9 +121,9 @@ public class VariablesTheme {
 
         System.out.println("\n 9. Перевод секунд в часы, минуты и секунды.");
         int seconds = 86399;
-        int clockHours = seconds / 3600;
-        int clockMin = seconds / 60 % 60;
-        int clockSec = seconds % 60;
-        System.out.println(clockHours + ":" + clockMin + ":" + clockSec);
+        int hh = seconds / 3600;
+        int mm = seconds / 60 % 60;
+        int ss = seconds % 60;
+        System.out.println(hh + ":" + mm + ":" + ss);
     }
 }
